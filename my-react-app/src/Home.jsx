fe75d2c5ad178ff940c8d511536353329b0c2cb5
kartikay-28 <kartikayrattan1@gmail.com>
2025-02-28 08:52:30 +0530

    Created react app

---
+++ my-react-app/src/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h1 className="text-2xl font-bold">Kartikay Rattan</h1>
+            <p className="mt-2">Contact: kartikayrattan1@gmail.com</p>
+            <p>Phone: +91 8219490447</p>
+        </div>
+    );
+};
+
+export default Home;
